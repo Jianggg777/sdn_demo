06c7ac5778d3ee43e3a87d8c7957a1a6a7868eb6
Jianggg777 <newstart0614@gmail.com>
2017-11-28 21:14:12 +0800

    pm web (mobile ver.) OK

--- pm_web/public/scripts/script.js
+++ pm_web/public/scripts/script.js
@@ -15,7 +15,39 @@ $(document).ready(function() {
 });
 
 function dynamicData(area) {
-
     console.log("hi???");
     console.log("area???"+area);
+    var objects = [];
+    $.ajax({
+        url: "/api/taiwan/"+area,
+        type: "GET",
+        //↓天辣辣辣辣這行太神了!!!!傳回值終於可以存惹感動
+        async: false,
+        success: function(data) {
+            /*
+            for (i = 0; i < data.length; i++) {
+                var inner = {
+                    "area": data[i].area, //資管
+                    "message": data[i].message, //女籃
+                }
+                objects.push(inner);
+            }
+            */
+            objects = data;
+            //console.log(objects);        
+        }
+    });
+    //塞進div
+    //$("#tw_content").append("<p >" + object.area + "：" + object.message+ "</p>");
+    var str = "<p>";
+    var i;
+    for(i=0;i<objects.length;i++){
+        str += objects[i].area + " " + objects[i].message+ "<br>";
+
+    }
+    str += "</p>";
+    $("#tw_content").html(str);
+    // 顯示
+    //document.getElementById('tw_button').click();
+    $( "#tw_button" ).click();
 }
--- pm_web/routes/index.js
+++ pm_web/routes/index.js
@@ -1,9 +1,27 @@
-exports.index = function ( req, res ){
-   res.render('index');
+exports.index = function(req, res) {
+    res.render('index');
 };
-exports.twData = function ( req, res ){
-   var area = req.params.area;
-   console.log("area :"+area);
-   // send the area data
-   res.render('index');
+exports.twData = function(req, res) {
+    var area = req.params.area;
+    console.log("area :" + area);
+    var data = "MESSAGE";
+    // send the area data
+    /*
+    res.json({
+        area: req.params.area,
+        message: 'The get api : ' + data
+    });*/
+    var objects = [];
+    var js = {
+        area: req.params.area,
+        message: 'The get api : ' + data
+    };
+    var js2 = {
+        area: "wow",
+        message: "HI"
+    };
+    objects.push(js);
+    objects.push(js2);
+    objects.push(js);
+    res.json(objects);
 };
